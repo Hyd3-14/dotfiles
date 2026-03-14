cfb74e7fda4665203d5c707778d6ee7c8422bc37
Your Name <your.email@example.com>
2026-03-14 09:33:58 +0900

    feat(config): Atcoder CLIとGHの設定ファイルを追加
    - Atcoder CLI用の設定ファイルを新規作成し、必要な設定を追加
    - GHの設定ファイルを新規作成し、エディタやプロンプト設定を追加
    - GWQとMiseの設定ファイルを新規作成し、ツールのバージョンを指定

---
+++ config/atcoder-cli-nodejs/cpp/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+using namespace std;
+
+template <typename T> using vec = vector<T>;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vvi = vec<vi>;
+using vvl = vec<vl>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+#define rep(i, x, limit) for (int i = (int)x; i < (int)limit; i++)
+#define REP(i, x, limit) for (int i = (int)x; i <= (int)limit; i++)
+#define irep(i, x, limit) for (int i = (int)x; i > (int)limit; i--)
+#define IREP(i, x, limit) for (int i = (int)x; i >= (int)limit; i--)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define el '\n'
+#define spa " "
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define YES cout << "YES" << el
+#define NO cout << "NO" << el
+#define eps (1e-10)
+#define Equals(a, b) (fabs((a) - (b)) < eps)
+#define debug(x) cerr << #x << " = " << x << el
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+}
